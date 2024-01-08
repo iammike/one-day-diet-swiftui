97b29a5c4b5b4d67191e0354448fb8947c53d915
Michael Collins <mike@iammike.org>
2024-01-07 19:46:43 -0500

    Saving data per day

--- One Day Diet/ContentView.swift
+++ One Day Diet/ContentView.swift
@@ -9,17 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
+    @State private var selectedDate = Date()
 
     var body: some View {
         VStack {
             Text("One Day Diet").font(.largeTitle).padding()
+            
             Text("Total Score: \(viewModel.calculateTotalScore())").font(.title)
-
-            ResetButton(action: viewModel.resetServings, label: "Clear Data")
+            
+            HStack {
+                DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                    .onChange(of: selectedDate) {
+                        viewModel.updateData(for: selectedDate)
+                    }
+                    .padding()
+                Spacer()
+                ResetButton(action: viewModel.resetServings, label: "Clear Data").padding()
+            }
 
             List {
                 ForEach(0..<foodGroupsData.count, id: \.self) { index in
                     FoodGroupSliderView(foodGroup: foodGroupsData[index], serving: $viewModel.selectedServings[index])
+                        .onReceive(viewModel.$selectedServings) { _ in
+                            viewModel.sliderValueChanged(on: selectedDate)
+                        }
                 }
             }
         }
@@ -27,7 +40,7 @@ struct ContentView: View {
 }
 
 @main
-struct DietApp: App {
+struct OneDayDiet: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
--- One Day Diet/ViewModel.swift
+++ One Day Diet/ViewModel.swift
@@ -9,18 +9,26 @@ import SwiftUI
 
 struct UserDefaultsKeys {
     static let selectedServings = "selectedServings"
+    static let servingsDataStore = "servingsDataStore"
 }
 
 class ViewModel: ObservableObject {
     @Published var selectedServings: [Int]
+    private var servingsDataStore: [String: [Int]] = [:]
 
     init() {
         self.selectedServings = UserDefaults.standard.array(forKey: UserDefaultsKeys.selectedServings) as? [Int] ?? Array(repeating: 0, count: foodGroupsData.count)
+        if let storedData = UserDefaults.standard.object(forKey: UserDefaultsKeys.servingsDataStore) as? [String: [Int]] {
+            self.servingsDataStore = storedData
+        }
+        let todayKey = Date().formattedDate
+        self.selectedServings = self.servingsDataStore[todayKey] ?? Array(repeating: 0, count: foodGroupsData.count)
     }
+
 
     func resetServings() {
         selectedServings = Array(repeating: 0, count: foodGroupsData.count)
-        UserDefaults.standard.set(selectedServings, forKey: UserDefaultsKeys.selectedServings)
+        saveData(for: Date().formattedDate)
     }
 
     func calculateTotalScore() -> Int {
@@ -28,4 +36,32 @@ class ViewModel: ObservableObject {
             total + foodGroupsData[index.offset].scores[index.element]
         }
     }
+
+    func updateData(for date: Date) {
+        let dateKey = date.formattedDate
+        if let savedData = servingsDataStore[dateKey] {
+            selectedServings = savedData
+        } else {
+            resetServings()
+        }
+        saveData(for: dateKey)
+    }
+
+    func saveData(for dateKey: String) {
+        servingsDataStore[dateKey] = selectedServings
+        UserDefaults.standard.set(servingsDataStore, forKey: UserDefaultsKeys.servingsDataStore)
+    }
+    
+    func sliderValueChanged(on date: Date) {
+        saveData(for: date.formattedDate)
+    }
 }
+
+extension Date {
+    var formattedDate: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.string(from: self)
+    }
+}
+
