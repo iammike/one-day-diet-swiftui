338f1d827357319bebd673ea59a84f24597e5530
Michael Collins <mike@iammike.org>
2024-01-11 12:50:32 -0500

    Improve FAQ

--- One Day Diet/FaqView.swift
+++ One Day Diet/FaqView.swift
@@ -35,7 +35,11 @@ struct FaqView: View {
                     
                     Spacer()
                     
-                    Text("**What should x, y, or z count as?** Use your best judgement, and again, consistency is all that's important. If something is made up of three parts from different categories, but only a single serving, only log a single serving. Which? Whichever matches how you feel this whole should count.")
+                    Text("**What should x, y, or z count as?** Use your best judgement, and again, consistency is all that's important.")
+                    
+                    Spacer()
+                    
+                    Text("**X food is made up of three different things, but its quantity is only one serving. How do I log it?** Use your best judgement based on the points you think it should earn or cost. If you can choose a group that aligns fairly well with what is contained in the food, even better.")
                     
                     Spacer()
                     
